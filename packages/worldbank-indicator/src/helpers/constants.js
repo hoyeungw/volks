78af6142d52af05607785fd2e39e14324ff5e521
Hoyeung <hoyeungw@outlook.com>
2021-06-28 12:26:48 +0800

    update configs

--- packages/worldbank-indicator/src/helpers/constants.js
+++ packages/worldbank-indicator/src/helpers/constants.js
@@ -3,4 +3,4 @@ export const BASE = 'http://api.worldbank.org/v2'
 export const COUNTRIES = ['USA', 'CHN', 'JPN', 'ECS'] // 'ECS': Europe & Central Asia
 export const GDP = 'NY.GDP.MKTP.CD', POP = 'SP.POP.TOTL'
 export const INDICATORS = [GDP, POP]
-export const WITHIN_5_YEARS = [2015, 2020]
+export const WITHIN_5_YEARS = [2015, 2021]
