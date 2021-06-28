785266977d54538806f3883a6a8e66aeabacb064
Hoyeung <hoyeungw@outlook.com>
2021-06-28 10:51:26 +0800

    update babel configs

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,18 @@
 module.exports = function (api) {
   api.cache(true)
-  const presets = ['@babel/preset-env']
-  const plugins = [
-    ['@babel/plugin-proposal-class-properties'],
-    ['@babel/plugin-proposal-private-methods'],
-    ['@babel/plugin-proposal-nullish-coalescing-operator'],
-    ['@babel/plugin-proposal-optional-chaining'],
-    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-    ['@babel/transform-runtime', { helpers: false }]
+  const presets = [
+    [ '@babel/preset-env', { targets: { node: '14' }, loose: true } ]
   ]
-  const ignore = ['node_modules/**']
+  const plugins = [
+    // [ '@babel/plugin-proposal-throw-expressions' ],
+    // [ '@babel/plugin-proposal-optional-chaining' ],
+    // [ '@babel/plugin-proposal-nullish-coalescing-operator' ],
+    [ '@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' } ],
+    // [ '@babel/plugin-proposal-class-properties', { loose: true } ],
+    // [ '@babel/plugin-proposal-private-methods', { loose: true } ],
+    // [ '@babel/plugin-transform-runtime', { helpers: false, } ]
+  ]
+  const ignore = [ 'node_modules/**' ]
   return {
     presets,
     plugins,
--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import babel               from '@rollup/plugin-babel'
 import commonjs            from '@rollup/plugin-commonjs'
+import json                from '@rollup/plugin-json'
 import nodeResolve         from '@rollup/plugin-node-resolve'
 import { decoObject, ros } from '@spare/logger'
 import fileInfo            from 'rollup-plugin-fileinfo'
@@ -7,23 +8,7 @@ import fileInfo            from 'rollup-plugin-fileinfo'
 const { name, dependencies, main, module } = require(process.cwd() + '/package.json')
 
 console.log(ros('Executing'), name, process.cwd())
-console.log(ros('Dependencies'), decoObject(dependencies || {}, { bracket: true }))
-
-const babelPluginOptions = {
-  babelrc: false,
-  comments: true,
-  sourceMap: true,
-  exclude: 'node_modules/**',
-  babelHelpers: 'bundled',
-  plugins: [
-    ['@babel/plugin-transform-runtime', { helpers: false, }],
-    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-    ['@babel/plugin-proposal-nullish-coalescing-operator'],
-    ['@babel/plugin-proposal-class-properties'],
-    ['@babel/plugin-proposal-private-methods'],
-    ['@babel/plugin-proposal-optional-chaining']
-  ]
-}
+console.log(ros('Dependencies'), decoObject(dependencies || {}))
 
 export default [
   {
@@ -36,8 +21,25 @@ export default [
     plugins: [
       nodeResolve({ preferBuiltins: true }),
       commonjs({ include: /node_modules/ }),
-      babel(babelPluginOptions),
+      babel({
+        babelrc: false,
+        comments: true,
+        sourceMap: true,
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+        presets: [ [ '@babel/preset-env', { targets: { node: '14' }, loose: true } ] ],
+        plugins: [
+          // [ '@babel/plugin-proposal-throw-expressions' ],
+          // [ '@babel/plugin-proposal-optional-chaining' ],
+          // [ '@babel/plugin-proposal-nullish-coalescing-operator' ],
+          [ '@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' } ],
+          // [ '@babel/plugin-proposal-class-properties', { loose: true } ],
+          // [ '@babel/plugin-proposal-private-methods', { loose: true } ],
+          // [ '@babel/plugin-transform-runtime', { helpers: false, } ]
+        ]
+      }),
       // terser(),
+      json(),
       fileInfo()
     ]
   }
